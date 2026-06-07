54ff6e378db57d27edd361b6567470a7e7ce6345
jors-hi-sys-de <jors@hi-sys.de>
2026-06-07 21:53:49 +0200

    Favicon für IOS

--- server.js
+++ server.js
@@ -42,12 +42,14 @@ const PUBLIC_FILES = new Map([
   ["/styles.css", "styles.css"],
   ["/favicon.ico", null],
   ["/favicon.svg", "favicon.svg"],
+  ["/sun-snow.png", "sun-snow.png"],
 ]);
 const CONTENT_TYPES = {
   ".css": "text/css; charset=utf-8",
   ".html": "text/html; charset=utf-8",
   ".js": "application/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
   ".svg": "image/svg+xml",
   ".txt": "text/plain; charset=utf-8",
 };
